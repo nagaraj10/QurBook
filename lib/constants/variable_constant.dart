42e845d635590c8003d0ec2358c762eb05de6402
Ravindran <ravindran.perumal@globalmantrai.com>
2020-08-14 16:34:03 +0530

    FHB-828 dashboard UI changes

--- lib/constants/variable_constant.dart
+++ lib/constants/variable_constant.dart
@@ -4,7 +4,6 @@ import 'package:flutter/services.dart';
 import 'package:myfhb/common/PreferenceUtil.dart';
 import 'package:myfhb/constants/fhb_constants.dart' as Constants;
 
-
 const String strAPP_NAME = 'MyFHB';
 const String strAppPackage = 'com.globalmantrainnovations.myfhb';
 const String strHealthRecordChannel = 'Health Record channel';
@@ -155,6 +154,7 @@ const String icon_digit_reco = 'assets/settings/digit_recognition.png';
 const String icon_device_recon = 'assets/settings/device_recognition.png';
 const String icon_provider = 'assets/navicons/my_providers.png';
 const String icon_records = 'assets/navicons/records.png';
+const String icon_chat = 'assets/navicons/chat.png';
 const String icon_th = 'assets/navicons/th.png';
 const String icon_home = 'assets/navicons/home.png';
 const String icon_schedule = 'assets/navicons/schedule.png';
@@ -178,7 +178,7 @@ const String font_roboto = 'Roboto';
 const String strBookmarkRecord = 'bookmark record';
 
 //for feedback
-const String strChat = 'chat';
+const String strChat = 'Chat';
 const String strFeedBack = 'Feedback';
 const String strFeedbackExp =
     'We would like to hear from you on your experience with MyFHB';
@@ -367,8 +367,6 @@ const String strGetProfileData = 'Getting ProfileData';
 
 String authToken;
 
-
-
 const String straccept = 'accept';
 const String strContentType = 'Content-Type';
 const String strAuthorization = 'Authorization';
@@ -421,7 +419,6 @@ const String strEmailOpt = 'Email address (optional)';
 const String strSendOtp = 'Send OTP';
 const String strEmailAddress = 'Email address';
 
-
 List<String> genderList = ['Male', 'Female', 'Others'];
 const String strEnterMobileNum = 'Enter MobileNumber';
 const String strEnterFirstname = 'Enter First Name';
@@ -452,7 +449,6 @@ const String strFalse = 'false';
 const String strSkip = 'Skip';
 const String strImgNtClear = 'Image not clear';
 
-
 //for device screen
 const String strGlucUnit = 'mgdl';
 const String strbfood = 'Before Food';
@@ -479,7 +475,6 @@ const String strWS = 'WS';
 const String strDT = 'DT';
 const String strPO = 'PO';
 const String strGL = 'GL';
-
 
 const String strBPMonitor = 'BP Monitor';
 const String strWeighingScale = 'Weighing Scale';
@@ -532,7 +527,8 @@ const String st_pausedplayer = 'paused player';
 const String strNoData = 'No data Available';
 const String strViewPDF = 'View PDF';
 const String strLogout = 'Logout';
-const String strLogoutMsg = 'Stay Healthy.. See you Soon. \nMaya will be waiting to serve you.';
+const String strLogoutMsg =
+    'Stay Healthy.. See you Soon. \nMaya will be waiting to serve you.';
 const String strYes = 'Yes';
 const String strRelaoding = 'Relaoding';
 const String strStartNow = 'Start now';
@@ -595,6 +591,3 @@ String _decodeBase64(String str) {
   }
   return utf8.decode(base64Url.decode(output));
 }
-
-
-
