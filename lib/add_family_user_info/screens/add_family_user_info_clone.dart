ad12bc2d371665831065d239bac1bc73621a30a3
Ravindran <ravindran.perumal@globalmantrai.com>
2020-11-06 10:59:58 +0530

    FHB-828 signup privacy policy included

--- lib/add_family_user_info/screens/add_family_user_info_clone.dart
+++ lib/add_family_user_info/screens/add_family_user_info_clone.dart
@@ -468,7 +468,7 @@ class AddFamilyUserInfoScreenState extends State<AddFamilyUserInfoScreen> {
           inputFormatters: (textEditingController == firstNameController ||
                   textEditingController == lastNameController ||
                   textEditingController == middleNameController)
-              ? [FilteringTextInputFormatter.allow(RegExp("[a-zA-Z]"))]
+              ? [WhitelistingTextInputFormatter(RegExp("[a-zA-Z]"))]
               : [],
         ));
   }
@@ -1299,9 +1299,9 @@ class AddFamilyUserInfoScreenState extends State<AddFamilyUserInfoScreen> {
       //allow only user who logged in to update their email address
       if (widget
           .arguments?.myProfileResult?.userContactCollection3.isNotEmpty) {
-        UserContactCollection3 userContact = widget
-          .arguments?.myProfileResult?.userContactCollection3[0];
-          userContact.email= emailController.text;
+        UserContactCollection3 userContact =
+            widget.arguments?.myProfileResult?.userContactCollection3[0];
+        userContact.email = emailController.text;
         List<UserContactCollection3> userContactCollection3List = new List();
         userContactCollection3List.add(userContact);
         profileResult.userContactCollection3 = userContactCollection3List;
