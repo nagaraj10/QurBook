e781cb235dd6788b3e7467c99fc630515b9ed26b
sankar <sankarsrec@gmail.com>
2021-03-31 16:51:39 +0530

    FHB-2639 - Schedule Dialog UI

---
+++ lib/device_integration/view/widgets/event_time_tile.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:myfhb/src/utils/screenutils/size_extensions.dart';
+import 'package:myfhb/common/CommonUtil.dart';
+
+class EventTimeTile extends StatefulWidget {
+  const EventTimeTile({
+    @required this.icon,
+    @required this.title,
+    @required this.onTimeSelected,
+  });
+
+  final IconData icon;
+  final String title;
+  final Function onTimeSelected;
+
+  @override
+  _EventTimeTileState createState() => _EventTimeTileState();
+}
+
+class _EventTimeTileState extends State<EventTimeTile> {
+  TimeOfDay timeSelected;
+  String selectedTime = '07:00';
+  DayPeriod selectedTimePeriod = DayPeriod.am;
+
+  getTimeAsString(TimeOfDay timeOfDay) {
+    return '${timeOfDay.hourOfPeriod > 9 ? '' : '0'}${timeOfDay.hourOfPeriod}:${timeOfDay.minute > 9 ? '' : '0'}${timeOfDay.minute}';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 0.75.sw,
+      padding: EdgeInsets.symmetric(
+        horizontal: 10.0.w,
+        vertical: 5.0.h,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Icon(
+                widget.icon,
+                color: Color(
+                  CommonUtil().getMyPrimaryColor(),
+                ),
+              ),
+              SizedBox(
+                width: 10.0.w,
+              ),
+              Text(
+                widget.title,
+                style: TextStyle(
+                  fontSize: 16.0.sp,
+                  color: Colors.black54,
+                ),
+              ),
+            ],
+          ),
+          Spacer(),
+          Row(
+            children: [
+              Container(
+                width: 50.0.w,
+                child: InkWell(
+                  onTap: () async {
+                    timeSelected = await showTimePicker(
+                      context: context,
+                      initialTime: timeSelected ?? TimeOfDay.now(),
+                    );
+                    if (timeSelected != null) {
+                      setState(() {
+                        selectedTimePeriod = timeSelected.period;
+                        selectedTime = getTimeAsString(timeSelected);
+                      });
+                      widget.onTimeSelected(timeSelected);
+                    }
+                  },
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        selectedTime,
+                        style: TextStyle(
+                          fontSize: 16.0.sp,
+                        ),
+                      ),
+                      Divider(
+                        height: 2.0.h,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 5.0.w,
+              ),
+              DropdownButton(
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 16.0.sp,
+                ),
+                underline: SizedBox.shrink(),
+                icon: Icon(
+                  Icons.keyboard_arrow_down,
+                  size: 24.0.sp,
+                ),
+                value: selectedTimePeriod,
+                onChanged: (value) {
+                  setState(() {
+                    selectedTimePeriod = value;
+                  });
+                },
+                items: [
+                  DropdownMenuItem(
+                    value: DayPeriod.am,
+                    child: Text('AM'),
+                  ),
+                  DropdownMenuItem(
+                    value: DayPeriod.pm,
+                    child: Text('PM'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/device_integration/view/widgets/regiment_tab.dart
+++ lib/device_integration/view/widgets/regiment_tab.dart
@@ -4,6 +4,7 @@ import 'package:myfhb/common/CommonUtil.dart';
 import 'package:provider/provider.dart';
 import 'package:myfhb/device_integration/viewModel/Device_model.dart';
 import 'regiment_data_card.dart';
+import 'event_time_tile.dart';
 
 class RegimentTab extends StatelessWidget {
   @override
@@ -19,41 +20,149 @@ class RegimentTab extends StatelessWidget {
               padding: EdgeInsets.all(10.0.sp),
               color: Colors.white,
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  InkWell(
-                    child: Icon(
-                      Icons.chevron_left_rounded,
-                      size: 24.0.sp,
-                      color: Color(CommonUtil().getMyPrimaryColor()),
+                  Row(
+                    children: [
+                      InkWell(
+                        child: Icon(
+                          Icons.chevron_left_rounded,
+                          size: 24.0.sp,
+                          color: Color(CommonUtil().getMyPrimaryColor()),
+                        ),
+                        onTap: () {
+                          Provider.of<DevicesViewModel>(context, listen: false)
+                              .getRegimentDate(isPrevious: true);
+                        },
+                      ),
+                      SizedBox(
+                        width: 5.0.w,
+                      ),
+                      Text(
+                        '${Provider.of<DevicesViewModel>(context).regimentDate}',
+                        style: TextStyle(
+                          fontSize: 14.0.sp,
+                          color: Color(CommonUtil().getMyPrimaryColor()),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 5.0.w,
+                      ),
+                      InkWell(
+                        child: Icon(
+                          Icons.chevron_right_rounded,
+                          size: 24.0.sp,
+                          color: Color(CommonUtil().getMyPrimaryColor()),
+                        ),
+                        onTap: () {
+                          Provider.of<DevicesViewModel>(context, listen: false)
+                              .getRegimentDate(isNext: true);
+                        },
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      right: 5.0.w,
                     ),
-                    onTap: () {
-                      Provider.of<DevicesViewModel>(context, listen: false)
-                          .getRegimentDate(isPrevious: true);
-                    },
-                  ),
-                  SizedBox(
-                    width: 5.0.w,
-                  ),
-                  Text(
-                    '${Provider.of<DevicesViewModel>(context).regimentDate}',
-                    style: TextStyle(
-                      fontSize: 14.0.sp,
-                      color: Color(CommonUtil().getMyPrimaryColor()),
+                    child: InkWell(
+                      onTap: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return StatefulBuilder(
+                                builder: (context, setState) {
+                              return SimpleDialog(
+                                children: [
+                                  EventTimeTile(
+                                    title: 'Wake up time',
+                                    icon: Icons.king_bed_rounded,
+                                    onTimeSelected: (timeSelected) {
+                                      print(timeSelected);
+                                    },
+                                  ),
+                                  EventTimeTile(
+                                    title: 'Breakfast',
+                                    icon: Icons.fastfood_rounded,
+                                    onTimeSelected: (timeSelected) {
+                                      print(timeSelected);
+                                    },
+                                  ),
+                                  EventTimeTile(
+                                    title: 'Break',
+                                    icon: Icons.free_breakfast,
+                                    onTimeSelected: (timeSelected) {
+                                      print(timeSelected);
+                                    },
+                                  ),
+                                  EventTimeTile(
+                                    title: 'Lunch time',
+                                    icon: Icons.fastfood_outlined,
+                                    onTimeSelected: (timeSelected) {
+                                      print(timeSelected);
+                                    },
+                                  ),
+                                  EventTimeTile(
+                                    title: 'Tea',
+                                    icon: Icons.emoji_food_beverage,
+                                    onTimeSelected: (timeSelected) {
+                                      print(timeSelected);
+                                    },
+                                  ),
+                                  EventTimeTile(
+                                    title: 'Dinner',
+                                    icon: Icons.food_bank,
+                                    onTimeSelected: (timeSelected) {
+                                      print(timeSelected);
+                                    },
+                                  ),
+                                  EventTimeTile(
+                                    title: 'Sleep time',
+                                    icon: Icons.bedtime_sharp,
+                                    onTimeSelected: (timeSelected) {
+                                      print(timeSelected);
+                                    },
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Container(
+                                        width: 150.0.w,
+                                        child: RaisedButton(
+                                          child: Text(
+                                            'OK',
+                                            style: TextStyle(
+                                              fontSize: 16.0.sp,
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                          color: Color(
+                                              CommonUtil().getMyPrimaryColor()),
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(
+                                              5.0.sp,
+                                            )),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                                contentPadding: EdgeInsets.all(10.0.sp),
+                              );
+                            });
+                          },
+                        );
+                      },
+                      child: Icon(
+                        Icons.access_time,
+                        size: 24.0.sp,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 5.0.w,
-                  ),
-                  InkWell(
-                    child: Icon(
-                      Icons.chevron_right_rounded,
-                      size: 24.0.sp,
-                      color: Color(CommonUtil().getMyPrimaryColor()),
-                    ),
-                    onTap: () {
-                      Provider.of<DevicesViewModel>(context, listen: false)
-                          .getRegimentDate(isNext: true);
-                    },
                   ),
                 ],
               ),
