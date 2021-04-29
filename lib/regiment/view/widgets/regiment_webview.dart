2fbe680ef87edc4bc8cb8e8f3d2feb3dbcd5387b
sankar <sankarsrec@gmail.com>
2021-04-29 17:34:54 +0530

    FHB-2788 - Open Direct Video Link In Regiment WebView

--- lib/regiment/view/widgets/regiment_webview.dart
+++ lib/regiment/view/widgets/regiment_webview.dart
@@ -1,12 +1,10 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:myfhb/common/CommonUtil.dart';
 import 'package:myfhb/constants/fhb_constants.dart';
 import 'package:myfhb/widgets/GradientAppBar.dart';
 import 'package:myfhb/src/utils/screenutils/size_extensions.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:myfhb/constants/variable_constant.dart' as variable;
+import 'package:flutter_html/flutter_html.dart';
 
 class RegimentWebView extends StatefulWidget {
   final String title;
@@ -60,41 +58,14 @@ class _RegimentWebViewState extends State<RegimentWebView> {
         ),
         body: Stack(
           children: <Widget>[
-            WebView(
-              initialUrl: widget.selectedUrl,
-              javascriptMode: JavascriptMode.unrestricted,
-              onWebViewCreated: (WebViewController webViewController) {
-                _controller = webViewController;
-                _loadHtmlFromAssets(widget.selectedUrl);
-              },
-              onPageFinished: (_) {
-                setState(() {
-                  isLoading = false;
-                });
+            Html(
+              data: widget.selectedUrl.replaceAll('src="//', 'src="'),
+              onLinkTap: (linkUrl) {
+                CommonUtil().openWebViewNew(widget.title, linkUrl, false);
+                print(linkUrl);
               },
             ),
-            isLoading
-                ? Center(
-                    child: CircularProgressIndicator(
-                    backgroundColor: Color(CommonUtil().getMyPrimaryColor()),
-                  ))
-                : Container(),
           ],
         ));
-  }
-
-  _loadHtmlFromAssets(String selectedUrl) async {
-    try {
-      if (selectedUrl.isNotEmpty) {
-        _controller.loadUrl(
-          Uri.dataFromString(selectedUrl,
-                  mimeType: variable.strtexthtml,
-                  encoding: Encoding.getByName(variable.strUtf))
-              .toString(),
-        );
-      }
-    } catch (e) {
-      print(e);
-    }
   }
 }
