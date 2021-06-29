ab76256c2960905d68bf086d87b8bdf4911315eb
AzureAD\MohamedFaisal <mohamed.faisal@qurhealth.in>
2021-06-29 14:02:05 +0530

    FHB-3251 added time request param in subscribe api

--- lib/plan_dashboard/services/subscribeService.dart
+++ lib/plan_dashboard/services/subscribeService.dart
@@ -3,14 +3,17 @@ import 'package:myfhb/plan_dashboard/model/CreateSubscribeModel.dart';
 import 'package:myfhb/plan_dashboard/model/subscribeModel.dart';
 import 'package:myfhb/src/resources/network/ApiBaseHelper.dart';
 import 'dart:convert' as convert;
+import 'package:intl/intl.dart';
 
 class SubscribeService {
   ApiBaseHelper _helper = ApiBaseHelper();
 
   Future<SubscribeModel> subscribePlan(String packageId,String patientId) async {
+    DateFormat dateFormat = DateFormat("yyyy-MM-dd HH:mm:ss");
+    String localTime = dateFormat.format(DateTime.now());
     var body = {};
     body['method'] = qr_get;
-    body['data'] = qr_subscribePlan+packageId+qr_patientEqaul+patientId;
+    body['data'] = qr_subscribePlan+packageId+qr_patientEqaul+patientId+qr_timeEqaul+localTime;
     var jsonString = convert.jsonEncode(body);
     final response = await _helper.getPlanList(qr_plan_list,jsonString);
     return SubscribeModel.fromJson(response);
