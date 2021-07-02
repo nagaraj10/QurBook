4d4c9403c6861aefa839dd14a8ea9b3f1217233a
ParvathiM <parvathi.m@qurhealth.in>
2021-07-02 20:53:06 +0530

    FHB-3630 when user tap the notification navigate to sheela ji

--- lib/telehealth/features/Notifications/model/messageDetails.dart
+++ lib/telehealth/features/Notifications/model/messageDetails.dart
@@ -1,8 +1,10 @@
+import 'package:myfhb/telehealth/features/Notifications/model/RawMessage.dart';
 import 'package:myfhb/telehealth/features/Notifications/model/content.dart';
 import 'package:myfhb/telehealth/features/Notifications/model/messageContent.dart';
 import 'package:myfhb/telehealth/features/Notifications/model/payload.dart';
 
 class MessageDetails {
+  RawMessage rawMessage;
   Content content;
   Payload payload;
   MessageContent messageContent;
@@ -12,6 +14,8 @@ class MessageDetails {
   MessageDetails.fromJson(Map<String, dynamic> json) {
     content =
     json['content'] != null ? new Content.fromJson(json['content']) : null;
+    rawMessage =
+    json['rawMessage'] != null ? new RawMessage.fromJson(json['rawMessage']) : null;
     payload =
     json['payload'] != null ? new Payload.fromJson(json['payload']) : null;
     messageContent = json['messageContent'] != null
@@ -23,6 +27,8 @@ class MessageDetails {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     if (this.content != null) {
       data['content'] = this.content.toJson();
+    }  if (this.rawMessage != null) {
+      data['rawMessage'] = this.rawMessage.toJson();
     }
     if (this.payload != null) {
       data['payload'] = this.payload.toJson();
