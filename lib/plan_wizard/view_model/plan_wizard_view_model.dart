ec70b304fd7ace0f48661d017c565a4e5ae9472f
Sankar Arumugam <sankar.arumugam@qurhealth.in>
2021-07-09 11:48:27 +0530

    FHB-3689 - Plan Search Wizard Base Screen View Model

--- lib/plan_wizard/view_model/plan_wizard_view_model.dart
+++ lib/plan_wizard/view_model/plan_wizard_view_model.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:myfhb/myPlan/model/myPlanListModel.dart';
 
 class PlanWizardViewModel extends ChangeNotifier {
   final PageController pageController = PageController();
   int currentPage = 0;
+  List<MyPlanListResult> carePlanList = [];
+  List<MyPlanListResult> dietPlanList = [];
+  List<MyPlanListResult> cartItemsList = [];
 
   void changeCurrentPage(int newPage) {
     currentPage = newPage;
+    notifyListeners();
+  }
+
+  void addCarePlan(MyPlanListResult newPlan) {
+    carePlanList.clear();
+    carePlanList.add(newPlan);
+    updateCart();
+  }
+
+  void addDietPlan(MyPlanListResult newPlan) {
+    dietPlanList.clear();
+    dietPlanList.add(newPlan);
+    updateCart();
+  }
+
+  void updateCart() {
+    cartItemsList.clear();
+    cartItemsList.addAll(carePlanList);
+    cartItemsList.addAll(dietPlanList);
     notifyListeners();
   }
 }
