d1913886a5fa551d502a9b5793493f4463c844fa
Sankar Arumugam <sankar.arumugam@qurhealth.in>
2021-07-27 20:21:01 +0530

    FHB-3868 - QurBook - Camera Issues Fixed

--- lib/src/ui/camera/TakePictureScreen.dart
+++ lib/src/ui/camera/TakePictureScreen.dart
@@ -169,7 +169,13 @@ class TakePictureScreenState extends State<TakePictureScreen> {
                 builder: (context, snapshot) {
                   if (snapshot.connectionState == ConnectionState.done) {
                     // If the Future is complete, display the preview.
-                    return CameraPreview(_controller);
+                    return Column(
+                      children: [
+                        Expanded(
+                          child: CameraPreview(_controller),
+                        ),
+                      ],
+                    );
                   } else {
                     // Otherwise, display a loading indicator.
                     return CommonCircularIndicator();
@@ -567,7 +573,7 @@ class TakePictureScreenState extends State<TakePictureScreen> {
       ).then((value) {
         categoryName =
             PreferenceUtil.getStringValue(Constants.KEY_CATEGORYNAME);
-        if (value) {
+        if (value ?? false) {
           Navigator.of(context).pop(true);
         }
       });
