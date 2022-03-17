d2210fdf6aae2a018606cfc4667c6e586fcc8cec
Venkatesh <venkatesh.n@qurhealth.in>
2022-03-17 13:35:39 +0530

    FHB-5050 done

--- lib/my_family/screens/MyFamily.dart
+++ lib/my_family/screens/MyFamily.dart
@@ -622,7 +622,7 @@ class _MyFamilyState extends State<MyFamily> {
 
   saveMediaDialog(BuildContext context) {
     _selectedDialogCountry =
-        CountryPickerUtils.getCountryByIsoCode('IN');
+        CountryPickerUtils.getCountryByIsoCode(CommonUtil.REGION_CODE);
     firstNameController.text = '';
     middleNameController.text = '';
     lastNameController.text = '';
