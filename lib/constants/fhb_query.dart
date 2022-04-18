e47be24b48124d1ba20d29c17e2c29fda5c2863a
ParvathiM <parvathi.m@qurhealth.in>
2022-04-18 12:44:38 +0530

    FHB-5117 alert msg delink

--- lib/constants/fhb_query.dart
+++ lib/constants/fhb_query.dart
@@ -355,3 +355,5 @@ const String qr_chat_socket_get_user_id_doc = 'doctor/';
 const String qr_chat_socket_get_user_id_doc_include = '?include=personal';
 
 const String qr_hub = 'qur-hub/user-hub';
+
+const String qr_delink_check="user-relationship/check-caregiver-association";
