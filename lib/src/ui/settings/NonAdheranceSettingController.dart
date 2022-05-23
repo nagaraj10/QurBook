6e5a08a5f315e1593504df49c303ec0d7b18b594
Anbarasanarumugam <anbarasan.arumugam@qurhealth.in>
2022-05-23 19:13:59 +0530

    feature/EFHB-3403 working on api integration

---
+++ lib/src/ui/settings/NonAdheranceSettingController.dart
@@ -0,0 +1,26 @@
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/get.dart';
+import 'package:myfhb/my_family/models/FamilyMembersRes.dart';
+import 'package:myfhb/my_family/models/FamilyMembersResponse.dart';
+import 'package:myfhb/my_family/services/FamilyMemberListRepository.dart';
+
+class NonAdheranceSettingController extends GetxController {
+  var loadingData = false.obs;
+  final familyRepository = FamilyMemberListRepository();
+
+  FamilyMembers familyResponseList;
+
+
+  getFamilyMemberList() async {
+    try {
+      loadingData.value = true;
+      familyResponseList = await familyRepository.getFamilyMembersListNew();
+      loadingData.value = false;
+      update(["newUpdate"]);
+    } catch (e) {
+    print(e.toString());
+    loadingData.value = false;
+    }
+  }
+
+}
---
+++ lib/src/ui/settings/NonAdheranceSettingsScreen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:get/get_state_manager/src/simple/get_state.dart';
+import 'package:myfhb/constants/fhb_constants.dart' as Constants;
+import 'package:myfhb/constants/fhb_constants.dart';
+import 'package:myfhb/my_family/models/FamilyMembersRes.dart';
+import 'package:myfhb/src/ui/settings/NonAdheranceSettingController.dart';
+import 'package:myfhb/src/utils/screenutils/size_extensions.dart';
+import 'package:myfhb/widgets/GradientAppBar.dart';
+
+class NonAdheranceSettingsScreen extends StatefulWidget {
+  const NonAdheranceSettingsScreen({Key key}) : super(key: key);
+
+  @override
+  _NonAdheranceSettingsScreenState createState() =>
+      _NonAdheranceSettingsScreenState();
+}
+
+class _NonAdheranceSettingsScreenState
+    extends State<NonAdheranceSettingsScreen> {
+  final controller = Get.put(NonAdheranceSettingController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        titleSpacing: 0.0,
+        title: Transform(
+          // you can forcefully translate values left side using Transform
+          transform: Matrix4.translationValues(-20.0, 0.0, 0.0),
+          child: Text(
+            Constants.careGiver,
+            style: TextStyle(fontSize: 20.sp),
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            size: 24.0.sp,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        flexibleSpace: GradientAppBar(),
+      ),
+      body: Obx(
+        () => controller.loadingData.isTrue
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : GetBuilder<NonAdheranceSettingController>(
+                id: "newUpdate",
+                builder: (val) {
+                  val.familyResponseList == null
+                      ? const Center(
+                          child: Text(
+                            'Please re-try after some time',
+                          ),
+                        )
+                      : val.familyResponseList.result.sharedByUsers != 0
+                          ? showData(
+                              val.familyResponseList.result.sharedByUsers)
+                          : const Center(
+                              child: Text(
+                                'No activities scheduled today',
+                              ),
+                            );
+                },
+              ),
+      ),
+    );
+  }
+
+  showData(List<SharedByUsers> sharedByUsers) {
+    return ListView.builder(
+        itemCount: sharedByUsers.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Row(
+              children: [
+                Text(sharedByUsers[index].nickName),
+
+              ],
+            ),
+          );
+        });
+  }
+}
