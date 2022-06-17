7791588f69436a13cd92b1f82912a74fe46b1a82
Anbarasanarumugam <anbarasan.arumugam@qurhealth.in>
2022-06-17 12:02:49 +0530

    feature/EFHB-3666 working on sheela wake

--- lib/my_family_detail/screens/my_family_detail_screen.dart
+++ lib/my_family_detail/screens/my_family_detail_screen.dart
@@ -204,9 +204,6 @@ class MyFamilyDetailScreenState extends State<MyFamilyDetailScreen> {
               IconButton(
                   icon: Icon(Icons.edit),
                   onPressed: () {
-                    if(widget.arguments.currentPage==null){
-
-                    }
                     if (widget.arguments.profilesSharedByMe != null &&
                         widget.arguments.profilesSharedByMe.length > 1) {
                       _currentPage = _currentPage + 1;
