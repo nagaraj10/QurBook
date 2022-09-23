ee70744a93e86c012b7a9bc576d1653fc6a29b1a
AzureAD\MohamedFaisal <mohamed.faisal@qurhealth.in>
2022-09-23 14:36:34 +0530

    EFHB-4947 android-ios-qurbook sheela bug fix

--- lib/src/ui/SheelaAI/Controller/SheelaAIController.dart
+++ lib/src/ui/SheelaAI/Controller/SheelaAIController.dart
@@ -43,6 +43,7 @@ class SheelaAIController extends GetxController {
   int indexOfCurrentPlayingConversation;
   AudioPlayer player;
   ScrollController scrollController = ScrollController();
+
   //Just make it true for using local tts,
   //rest of the cases will be auto handled .
   bool useLocalTTSEngine = false;
@@ -53,6 +54,7 @@ class SheelaAIController extends GetxController {
   String relationshipId;
   String conversationFlag;
   bool lastMsgIsOfButtons = false;
+
   @override
   void onInit() {
     super.onInit();
@@ -89,7 +91,11 @@ class SheelaAIController extends GetxController {
             }
           } else {
             stopTTS();
-            if (conversations.last.isEndOfConv) {
+            try {
+              if (conversations.last.isEndOfConv) {
+                gettingReposnseFromNative();
+              }
+            } catch (e) {
               gettingReposnseFromNative();
             }
           }
@@ -325,7 +331,11 @@ class SheelaAIController extends GetxController {
             gettingReposnseFromNative();
           } else {
             stopTTS();
-            if (conversations.last?.isEndOfConv ?? false) {
+            try {
+              if (conversations.last.isEndOfConv) {
+                gettingReposnseFromNative();
+              }
+            } catch (e) {
               gettingReposnseFromNative();
             }
           }
@@ -571,6 +581,7 @@ class SheelaAIController extends GetxController {
 
   DeviceStatus currentDeviceStatus = DeviceStatus();
   CreateDeviceSelectionModel createDeviceSelectionModel;
+
   setValues(GetDeviceSelectionModel getDeviceSelectionModel) {
     final selection = getDeviceSelectionModel.result[0];
     final prof = selection.profileSetting;
