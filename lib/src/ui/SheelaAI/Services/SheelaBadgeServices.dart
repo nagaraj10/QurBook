5c5954e8e5033cc949d73e2bde0f6b1779e7bf46
AzureAD\MohamedFaisal <mohamed.faisal@qurhealth.in>
2022-10-03 22:49:31 +0530

    EFHB-4923 sheela-reminders-unread-count--sheela-reminders--

--- lib/src/ui/SheelaAI/Services/SheelaBadgeServices.dart
+++ lib/src/ui/SheelaAI/Services/SheelaBadgeServices.dart
@@ -1,6 +1,4 @@
-import 'dart:convert' as convert;
 
-import 'package:myfhb/chat_socket/model/ChatHistoryModel.dart';
 import 'package:myfhb/chat_socket/model/SheelaBadgeModel.dart';
 import 'package:myfhb/common/PreferenceUtil.dart';
 import 'package:myfhb/constants/fhb_constants.dart';
@@ -13,7 +11,7 @@ class SheelaBadgeServices {
   Future<SheelaBadgeModel> getSheelaBadgeCount() async {
     String userId = PreferenceUtil.getStringValue(KEY_USERID);
     final response =
-    await _helper.getSheelaBadgeCount(qr_sheela_badge_icon_count+'03ae3077-4980-4120-9cb0-f1954a956415');
+    await _helper.getSheelaBadgeCount(qr_sheela_badge_icon_count+userId);
     return SheelaBadgeModel.fromJson(response);
   }
 
