06c83f463777d72380f7b598ec1948a4d6964381
ParvathiM <parvathi.m@qurhealth.in>
2022-12-29 16:25:42 +0530

    EFHB-5842 qurbook redirection when audio msg received

--- lib/telehealth/features/Notifications/view/notification_screen.dart
+++ lib/telehealth/features/Notifications/view/notification_screen.dart
@@ -1161,9 +1161,9 @@ class _NotificationScreen extends State<NotificationScreen> {
                   audioMessage:
                       result?.messageDetails?.payload?.sheelaAudioMsgUrl,
                 ),
-              ).then((value) =>
-                  PageNavigator.goToPermanent(context, router.rt_Landing));
-              readUnreadAction(result);
+              ).then((value) {
+                readUnreadAction(result, isRead: true);
+              });
             }
           } else {
             Get.to(SuperMaya());
