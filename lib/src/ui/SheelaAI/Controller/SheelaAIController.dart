c85720a17871be82d59f4b4810bf0d7b82e0f4c1
ParvathiM <parvathi.m@qurhealth.in>
2023-03-03 13:43:21 +0530

    EFHB-6649 sheela to remove lookup numbers

--- lib/src/ui/SheelaAI/Controller/SheelaAIController.dart
+++ lib/src/ui/SheelaAI/Controller/SheelaAIController.dart
@@ -462,7 +462,18 @@ class SheelaAIController extends GetxController {
             .isNotEmpty) {
           textForPlaying = currentButton.ttsResponse.payload.audioContent;
         } else if ((currentButton.title ?? '').isNotEmpty) {
-          final result = await getGoogleTTSForText(currentButton.title);
+          var result;
+          try {
+            var stringToSpeech = currentButton.title;
+            if (currentButton.title.contains(".")) {
+              stringToSpeech = currentButton.title.split(".")[1];
+            }
+
+            result = await getGoogleTTSForText(stringToSpeech);
+          } catch (e) {
+            result = await getGoogleTTSForText(currentButton.title);
+          }
+
           if ((result.payload?.audioContent ?? '').isNotEmpty) {
             textForPlaying = result.payload.audioContent;
           }
