b28cb37e49fbac0854325a35195af476fb300d0e
Senthilkumar <>
2023-03-07 16:20:05 +0530

    EFHB-6630 Conflicts solved

--- android/app/src/main/kotlin/com/ventechsolutions/myFHB/MainActivity.kt
+++ android/app/src/main/kotlin/com/ventechsolutions/myFHB/MainActivity.kt
@@ -554,19 +554,20 @@ class MainActivity : FlutterFragmentActivity(), SessionController.Listener,
                 runOnUiThread {
 //                Toast.makeText(applicationContext, "BP: "+p1.toString(), Toast.LENGTH_SHORT).show()
 
-                if (ActivityCompat.checkSelfPermission(
-                        applicationContext,
-                        Manifest.permission.BLUETOOTH_CONNECT
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return@runOnUiThread
+                    if (ActivityCompat.checkSelfPermission(
+                                    applicationContext,
+                                    Manifest.permission.BLUETOOTH_CONNECT
+                            ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return@runOnUiThread
+                    }
                 }
                 bleName = p0?.name
                 var bleMacId: String
--- lib/common/CommonUtil.dart
+++ lib/common/CommonUtil.dart
@@ -5633,6 +5633,30 @@ class CommonUtil {
     }
   }
 
+    String getFirstAndLastName(String strText) {
+    String strName = strText;
+    String strName1 = "";
+    String strName2 = "";
+    try {
+      if (strName.contains(" ")) {
+        strName1 = strName.split(" ").first;
+        strName2 = strName.split(" ").last;
+      } else {
+        strName1 = strName;
+      }
+      return strName2.trim().isNotEmpty
+          ? strName1[0].toUpperCase() + strName2[0].toUpperCase()
+          : strName1.trim().isNotEmpty
+              ? strName1[0].toUpperCase()
+              : "";
+    } catch (e) {
+      if (kDebugMode) {
+        printError(info: e.toString());
+      }
+    }
+    return strName.trim().isNotEmpty ? strName[0].toUpperCase() : "";
+  }
+
   AppointmentDetailsController onInitAppointmentDetailsController() {
     AppointmentDetailsController appointmentDetailsController;
     if (!Get.isRegistered<AppointmentDetailsController>()) {
