9132feaad0f408adb18dedfe255aee826c705963
AzureAD\MohamedFaisal <mohamed.faisal@qurhealth.in>
2023-03-15 16:39:11 +0530

    EFHB-7293 qurhome-to-show-just-the-first

--- lib/Qurhome/QurhomeDashboard/View/QurhomeDashboard.dart
+++ lib/Qurhome/QurhomeDashboard/View/QurhomeDashboard.dart
@@ -228,43 +228,47 @@ class _QurhomeDashboardState extends State<QurhomeDashboard> with RouteAware {
                           ))
                       : SizedBox.shrink(),
                   if (CommonUtil.isUSRegion()) SizedBox(width: 22.w),
-                  Column(
-                    children: [
-                      RichText(
-                        text: TextSpan(
-                          // Note: Styles for TextSpans must be explicitly defined.
-                          // Child text spans will inherit styles from parent
-                          style: TextStyle(
-                            fontSize: textFontSize,
-                            color: Colors.black,
-                          ),
-                          children: <TextSpan>[
-                            if (controller.currentSelectedIndex.value == 0 ||
-                                controller.currentSelectedIndex.value == 1) ...{
-                              TextSpan(text: 'Hello '),
-                            },
-                            TextSpan(
-                              text: controller.appBarTitle.value,
-                              style: TextStyle(
-                                  fontSize: textFontSize,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        RichText(
+                          overflow: TextOverflow.fade,
+                          softWrap: false,
+                          text: TextSpan(
+                            // Note: Styles for TextSpans must be explicitly defined.
+                            // Child text spans will inherit styles from parent
+                            style: TextStyle(
+                              fontSize: textFontSize,
+                              color: Colors.black,
                             ),
-                          ],
-                        ),
-                      ),
-                      if (controller.currentSelectedIndex.value == 0 ||
-                          controller.currentSelectedIndex.value == 1) ...{
-                        SizedBox(height: 3),
-                        Text(
-                          'Today, ' + qurHomeRegimenController.dateHeader.value,
-                          style: TextStyle(
-                            fontSize: 12.h,
-                            color: Colors.grey,
+                            children: <TextSpan>[
+                              if (controller.currentSelectedIndex.value == 0 ||
+                                  controller.currentSelectedIndex.value == 1) ...{
+                                TextSpan(text: 'Hello '),
+                              },
+                              TextSpan(
+                                text: controller.appBarTitle.value,
+                                style: TextStyle(
+                                    fontSize: textFontSize,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                            ],
                           ),
                         ),
-                      },
-                    ],
+                        if (controller.currentSelectedIndex.value == 0 ||
+                            controller.currentSelectedIndex.value == 1) ...{
+                          SizedBox(height: 3),
+                          Text(
+                            'Today, ' + qurHomeRegimenController.dateHeader.value,
+                            style: TextStyle(
+                              fontSize: 12.h,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        },
+                      ],
+                    ),
                   ),
                   if (CommonUtil.isUSRegion() &&
                       controller.currentSelectedIndex == 0)
