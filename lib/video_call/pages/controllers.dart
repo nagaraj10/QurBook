4d77f5b05d81bcaa0a5454f55ff5bce1474cccef
SadhamHussain <sadham.hussain@qurhealth.in>
2023-06-13 19:35:41 +0530

    EFHB-8941-mobile-support-for-call-discon call log fb method added.

--- lib/video_call/pages/controllers.dart
+++ lib/video_call/pages/controllers.dart
@@ -2,15 +2,14 @@ import 'dart:io';
 
 import 'package:agora_rtc_engine/rtc_engine.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:gmiwidgetspackage/widgets/flutterToast.dart';
 import 'package:myfhb/chat_socket/view/ChatDetail.dart';
 import 'package:myfhb/common/CommonUtil.dart';
 import 'package:myfhb/common/PreferenceUtil.dart';
-import 'package:myfhb/src/model/home_screen_arguments.dart';
 import 'package:myfhb/src/ui/SplashScreen.dart';
-import 'package:myfhb/telehealth/features/MyProvider/view/TelehealthProviders.dart';
 import 'package:myfhb/telehealth/features/chat/viewModel/ChatViewModel.dart';
 import 'package:myfhb/video_call/model/videocallStatus.dart';
 import 'package:myfhb/video_call/utils/audiocall_provider.dart';
@@ -329,23 +328,35 @@ class _MyControllersState extends State<MyControllers> {
   }
 
   void _onCallEnd(BuildContext context) async {
-    if (Platform.isIOS) {
-      if (PreferenceUtil.getCallNotificationReceived()) {
-        PreferenceUtil.setCallNotificationRecieved(isCalled: false);
-        Navigator.pushAndRemoveUntil(
-            context,
-            MaterialPageRoute(
-                builder: (context) => SplashScreen(isFromCallScreen: true)),
-            (route) => false);
+    try {
+      if (Platform.isIOS) {
+        if (PreferenceUtil.getCallNotificationReceived()) {
+          PreferenceUtil.setCallNotificationRecieved(isCalled: false);
+          Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => SplashScreen(isFromCallScreen: true)),
+              (route) => false);
+        } else {
+          Navigator.pop(context);
+        }
       } else {
-        Navigator.pop(context);
+        if (widget.isAppExists!) {
+          Navigator.pop(context);
+        } else {
+          Navigator.of(context).pushNamedAndRemoveUntil(
+              '/splashscreen', (Route<dynamic> route) => false);
+        }
       }
-    } else {
-      if (widget.isAppExists!) {
-        Navigator.pop(context);
-      } else {
-        Navigator.of(context).pushNamedAndRemoveUntil(
-            '/splashscreen', (Route<dynamic> route) => false);
+
+      await FirebaseFirestore.instance
+          .collection('call_log')
+          .doc(widget.channelName??"")
+          .delete();
+
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
       }
     }
   }
