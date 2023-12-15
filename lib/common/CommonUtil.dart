942a01d8e692bbabf1c7c6b8dd0cd72faebe13a9
AzureAD\MohamedFaisal <mohamed.faisal@qurhealth.in>
2023-12-15 16:42:03 +0530

    EFHB-14143 qurhomeus---when-activity-is-deleetd-from-qurplus

--- lib/common/CommonUtil.dart
+++ lib/common/CommonUtil.dart
@@ -1716,6 +1716,14 @@ class CommonUtil {
     return updatedDate;
   }
 
+  snoozeDataFormat(DateTime newDateTime) {
+    DateFormat newFormat;
+    var updatedDate = '';
+    newFormat = DateFormat('yyyy-MM-dd HH:mm:ss');
+    updatedDate = updatedDate + newFormat.format(newDateTime);
+    return updatedDate;
+  }
+
   regimentDateFormatV2(
     DateTime newDateTime, {
     bool isAck = false,
