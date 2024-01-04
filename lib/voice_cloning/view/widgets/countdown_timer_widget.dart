9b7d7f131b706a3b956a299e7f25d98bb72c6b2c
Venkatesh <Venkatesh.n@qurhealth.in>
2024-01-04 22:25:54 +0530

    EFHB-11547 minor import fixes

--- lib/voice_cloning/view/widgets/countdown_timer_widget.dart
+++ lib/voice_cloning/view/widgets/countdown_timer_widget.dart
@@ -1,13 +1,14 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:myfhb/common/CommonUtil.dart';
-import 'package:myfhb/constants/fhb_constants.dart';
-import 'package:myfhb/src/utils/screenutils/size_extensions.dart';
-import 'package:myfhb/voice_cloning/controller/voice_cloning_controller.dart';
 import 'package:provider/provider.dart';
 
+import '../../../common/CommonUtil.dart';
+import '../../../constants/fhb_constants.dart';
+import '../../../src/utils/screenutils/size_extensions.dart';
+import '../../controller/voice_cloning_controller.dart';
+
 class VoiceCloningCountDownWidget extends StatefulWidget {
+  const VoiceCloningCountDownWidget({super.key});
+
   @override
   State<VoiceCloningCountDownWidget> createState() =>
       _VoiceCloningCountDownWidgetState();
@@ -19,43 +20,50 @@ class _VoiceCloningCountDownWidgetState
 
   @override
   void initState() {
-    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       ///Starting the countdown timer once  dialog has visible.
-      Provider.of<VoiceCloningController>(context, listen: false).startCountDownTimer();
+      Provider.of<VoiceCloningController>(
+        context,
+        listen: false,
+      ).startCountDownTimer();
     });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    /// Retrieving and Listening to VoiceCloningController reference from the Provider
-      mControllerWatch =Provider.of<VoiceCloningController>(context);
+    /// Retrieving and Listening to VoiceCloningController
+    /// reference from the Provider
+    mControllerWatch = Provider.of<VoiceCloningController>(context);
     return Container(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
-      padding: EdgeInsets.all(6),
+      padding: const EdgeInsets.all(6),
       child: Column(
         children: [
           Align(
             alignment: Alignment.centerRight,
             child: InkWell(
-              onTap: ()=>Navigator.pop(context),
-              child: Icon(Icons.close,
-              color: Colors.white,
-              size:40,),
+              onTap: () => Navigator.pop(context),
+              child: const Icon(
+                Icons.close,
+                color: Colors.white,
+                size: 40,
+              ),
             ),
           ),
           Expanded(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
                   makeSureNotInNoiseDescription,
                   textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.white,
-                      fontSize: 14.sp,
-                      fontWeight: FontWeight.w600),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
                 SizedBox(
                   height: 20.h,
@@ -63,7 +71,8 @@ class _VoiceCloningCountDownWidgetState
                 Text(
                   readTheDisplayContentDescription,
                   textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.white,
+                  style: TextStyle(
+                      color: Colors.white,
                       fontSize: 14.sp,
                       fontWeight: FontWeight.w600),
                 ),
@@ -76,11 +85,14 @@ class _VoiceCloningCountDownWidgetState
                     Container(
                       height: 100.h,
                       width: 100.h,
-                      child:CircularProgressIndicator(
+                      child: CircularProgressIndicator(
                         strokeWidth: 10,
                         value: mControllerWatch.progressValue,
                         valueColor: AlwaysStoppedAnimation<Color>(
-                            Color(CommonUtil().getMyPrimaryColor())),
+                          Color(
+                            CommonUtil().getMyPrimaryColor(),
+                          ),
+                        ),
                         backgroundColor: Colors.white,
                       ),
                     ),
@@ -100,6 +112,7 @@ class _VoiceCloningCountDownWidgetState
       ),
     );
   }
+
   @override
   void dispose() {
     ///cancelling the countdown timer if dialog as disappears.
