d12f61a7a52a98f746804cef507f628e45fc1eb0
Mihir Pipermitwala <mihir.pipermitwala@qurhealth.in>
2024-01-12 09:53:52 +0530

    EFHB-15086 add API query parameters toogleVoiceClone

--- lib/src/resources/repository/health/HealthReportListForUserRepository.dart
+++ lib/src/resources/repository/health/HealthReportListForUserRepository.dart
@@ -437,7 +437,7 @@ class HealthReportListForUserRepository {
       'tags': tagsList
     });
     final response = await _helper.updateDeviceSelection(
-        query.qr_user_profile_no_slash, body);
+        query.qr_user_profile_no_slash + '?toogleVoiceClone=$voiceCloning', body);
     return UpdateDeviceModel.fromJson(response);
   }
 
