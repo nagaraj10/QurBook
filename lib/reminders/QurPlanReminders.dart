fb104a17798d298ad1b28ca4eec9d5c1eb45524e
AzureAD\MohamedFaisal <mohamed.faisal@qurhealth.in>
2024-01-23 20:47:16 +0530

    EFHB-15090 sheela-multiple-reminder-master

--- lib/reminders/QurPlanReminders.dart
+++ lib/reminders/QurPlanReminders.dart
@@ -365,28 +365,40 @@ class QurPlanReminders {
       return notifications;
     } catch (e, stackTrace) {
       try {
+        // Get SharedPreferences instance
         SharedPreferences prefs = await SharedPreferences.getInstance();
+
+        // Retrieve the JSON string from SharedPreferences
         String? jsonData = prefs.getString('remindersPref');
+
+        // Check if the JSON string is not null
         if (jsonData != null) {
-                json.decode(jsonData);
-              }
-        var retrievedData = await json.decode(jsonData!);
-        final List<dynamic> myJson = retrievedData['reminders'];
+          // Decode the JSON string into a Dart object
+          var retrievedData = await json.decode(jsonData);
 
-        notifications = <Reminder>[];
-        for (var i = 0; i < myJson.length; i++) {
-          var val = Reminder.fromJson(myJson[i]);
-          //final newData = Reminder.fromMap(val);
-          notifications.add(val);
+          // Extract the 'reminders' list from the decoded data
+          final List<dynamic> myJson = retrievedData['reminders'];
+
+          // Initialize an empty list to store Reminder objects
+          notifications = <Reminder>[];
+
+          // Iterate over the 'reminders' list and convert each item to a Reminder object
+          for (var i = 0; i < myJson.length; i++) {
+            // Convert the current JSON object to a Reminder object using the fromJson method
+            var val = Reminder.fromJson(myJson[i]);
+
+            // Add the converted Reminder object to the notifications list
+            notifications.add(val);
+          }
+
+          // Return the list of Reminder objects
+          return notifications;
         }
-        return notifications;
-      } catch (e) {
-        print(e);
+      } catch (e, stackTrace) {
+        // Handle any exceptions that may occur during the process
+        CommonUtil().appLogs(message: e, stackTrace: stackTrace);
       }
-      //CommonUtil().appLogs(message: e, stackTrace: stackTrace);
 
-      //print(e.toString());
-     // return [];
     }
     return notifications;
   }
