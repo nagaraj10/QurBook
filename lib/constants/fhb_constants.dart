6f783bdaf3df36448a09800c45fa0b79e2656c97
AzureAD\MohamedFaisal <mohamed.faisal@qurhealth.in>
2024-02-02 10:40:29 +0530

    EFHB-16158 sheelamedia-error-message-is-sheela

--- lib/constants/fhb_constants.dart
+++ lib/constants/fhb_constants.dart
@@ -974,7 +974,7 @@ const String strImageSizeValidation = 'Unable to upload photo. File size exceeds
 const String strVideoSizeValidation = 'Unable to upload video. File size exceeds 100MB';
 
 // Constant for the error message when audio file size exceeds 100MB
-const String strAudioSizeValidation = 'File size exceeds 100MB';
+const String strAudioSizeValidation = 'Unable to upload audio. File size exceeds 100MB';
 
 // Constant for the label 'Video preview'
 const String strVideoPreview = 'Video preview';
