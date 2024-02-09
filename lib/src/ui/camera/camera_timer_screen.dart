1a848775e9019f03c7d67b4c25e234a27951cab2
JotheeswariR <jotheeswari.ramasamy@qurhealth.in>
2024-02-09 09:13:54 +0530

    EFHB-15427 shutter sound added

--- lib/src/ui/camera/camera_timer_screen.dart
+++ lib/src/ui/camera/camera_timer_screen.dart
@@ -3,10 +3,14 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:gmiwidgetspackage/widgets/FlutterToast.dart';
+import 'package:native_shutter_sound/native_shutter_sound.dart';
+import 'package:native_shutter_sound/native_shutter_sound_platform_interface.dart';
+
+import '../../utils/screenutils/size_extensions.dart';
 
 class CameraPreviewScreen extends StatefulWidget {
-  const CameraPreviewScreen({Key? key, required this.cameras}) : super(key: key);
+  const CameraPreviewScreen({super.key, required this.cameras});
+
   final List<CameraDescription> cameras;
 
   @override
@@ -25,10 +29,12 @@ class _CameraPreviewScreenState extends State<CameraPreviewScreen> {
     cameraInitialization();
   }
 
-  void cameraInitialization() async {
-    controller = CameraController(widget.cameras[0], ResolutionPreset.medium);
+  Future<void> cameraInitialization() async {
+    controller = CameraController(
+      widget.cameras[0],
+      ResolutionPreset.high,
+    );
     await controller.initialize();
-
     _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       setState(() {
         if (_secondsRemaining > 0) {
@@ -50,13 +56,15 @@ class _CameraPreviewScreenState extends State<CameraPreviewScreen> {
 
   Future<void> _takePicture() async {
     try {
-      final XFile image = await controller.takePicture();
+      final image = await controller.takePicture();
+
       setState(() {
         imageFile = image;
       });
       _timer.cancel();
+      await NativeShutterSoundPlatform.instance.play();
     } catch (e) {
-      FlutterToast().getToast(e.toString(), Colors.red);
+      debugPrint(e.toString());
     }
   }
 
@@ -65,98 +73,109 @@ class _CameraPreviewScreenState extends State<CameraPreviewScreen> {
     if (!controller.value.isInitialized) {
       return Container();
     }
-      return _buildCameraPreview();
+    return _buildCameraPreview();
   }
 
   Widget _buildCameraPreview() => Scaffold(
-      backgroundColor: Colors.transparent,
-      bottomNavigationBar:  Padding(
-        padding: const EdgeInsets.all(20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Icon(
-                Icons.cancel_outlined,
-                size: 32,
-              ),
-            ),
-            Visibility(
-              visible: imageFile==null,
-              child: ElevatedButton(
-                onPressed: _takePicture,
-                child: const Icon(
-                  Icons.camera_alt_rounded,
-                  size: 32,
-                ),
-              ),
-            ),
-            Visibility(
-              visible: imageFile!=null,
-              child: IconButton(
-                onPressed: (){
-                  Navigator.of(context).pop(imageFile);
+        backgroundColor: Colors.transparent,
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
                 },
-                icon: const Icon(
-                  Icons.check,
-                  color: Colors.white,
-                  size: 32,
+                child: Icon(
+                  Icons.cancel_outlined,
+                  size: 32.0.sp,
                 ),
               ),
+              Visibility(
+                visible: imageFile == null,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 50),
+                  child: ElevatedButton(
+                    onPressed: _takePicture,
+                    child: Icon(
+                      Icons.camera_alt_rounded,
+                      size: 32.0.sp,
+                    ),
+                  ),
+                ),
+              ),
+              Visibility(
+                visible: imageFile != null,
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(imageFile);
+                  },
+                  icon: Icon(
+                    Icons.check,
+                    color: Colors.white,
+                    size: 32.0.sp,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: Stack(
+          children: [
+            Positioned.fill(
+              child: imageFile != null
+                  ? Image.file(
+                      File(imageFile!.path),
+                      fit: BoxFit.cover,
+                    )
+                  : CameraPreview(controller),
             ),
+            if (imageFile == null)
+              Positioned(
+                top: 50,
+                left: 0,
+                right: 0,
+                child: TweenAnimationBuilder<double>(
+                  tween: Tween<double>(begin: 1, end: 0),
+                  duration: Duration(seconds: _secondsRemaining),
+                  builder: (context, value, child) {
+                    // Calculate the color based on the remaining time
+                    Color color = _secondsRemaining <= 10 ? Colors.red : Colors.grey;
+                    return Container(
+                      width: 100,
+                      height: 100,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Container(
+                            width: 60,
+                            height: 60,
+                            // decoration: BoxDecoration(
+                            //   shape: BoxShape.circle,
+                            //   border: Border.all(color: color, width: 3),
+                            // ),
+                            child: CircularProgressIndicator(
+                              value: value,
+                              color: Colors.white,
+                              strokeWidth: 5,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 5.0), // Adjust the padding as needed
+                            child: Text(
+                              '$_secondsRemaining',
+                              style: const TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold // Adjust the color as needed
+                                  ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
           ],
         ),
-      ),
-      body:  Stack(
-        children: [
-          Positioned.fill(
-            child: imageFile != null
-                ? Image.file(
-              File(imageFile!.path),
-              fit: BoxFit.cover,
-            )
-                : CameraPreview(controller),
-          ),
-          if(imageFile == null)
-          Positioned(
-            top: 100,
-            left: 0,
-            right: 0,
-            child: Center(
-              child: Text(
-                '$_secondsRemaining',
-                style: const TextStyle(fontSize: 25, color: Colors.red),
-              ),
-            ),
-          ),
-
-        ],
-      ),
-    );
-
-
-
-  // void _retakePicture() {
-  //   setState(() {
-  //     imageFile = null;
-  //     _secondsRemaining = 30;
-  //   });
-  //   _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-  //     setState(() {
-  //       if (_secondsRemaining > 0) {
-  //         _secondsRemaining--;
-  //       } else {
-  //         _timer.cancel();
-  //         _takePicture();
-  //       }
-  //     });
-  //   });
-  // }
-
-  // void _acceptPicture() {
-  //   Navigator.of(context).pop(imageFile);
-  // }
+      );
 }
