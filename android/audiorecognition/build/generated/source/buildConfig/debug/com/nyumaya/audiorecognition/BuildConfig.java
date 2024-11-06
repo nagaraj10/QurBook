8c79febe79bd8ebe3323df41090006da993b2d4b
nagaraj <nagarajnr1997@gmail.com>
2024-11-06 20:03:22 +0530

    initial relase

---
+++ android/audiorecognition/build/generated/source/buildConfig/debug/com/nyumaya/audiorecognition/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.nyumaya.audiorecognition;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.nyumaya.audiorecognition";
+  public static final String BUILD_TYPE = "debug";
+}
